86dd4e6f65ad8e17bb0ddb1666a02e394b373a4e
Frosenow <filip.potepa@gmail.com>
2023-04-25 22:26:46 +0200

    created structure for storing dictionary

---
+++ src/autocomplete.tsx
@@ -0,0 +1,62 @@
+class TrieNode {
+  data: string;
+  isEnd: boolean;
+  children: Map<string, TrieNode>;
+
+  constructor(c: string) {
+    this.data = c;
+    this.isEnd = false;
+    this.children = new Map();
+  }
+}
+
+class Trie {
+  root: TrieNode;
+
+  constructor() {
+    this.root = new TrieNode("");
+  }
+
+  insert(word: string): void {
+    let node: TrieNode = this.root;
+    for (let char of word) {
+      if (!node.children.has(char)) {
+        node.children.set(char, new TrieNode(char));
+      }
+      node = node.children.get(char)!;
+    }
+    node.isEnd = true;
+  }
+
+  autocomplete(word: string): string[] {
+    let res: string[] = [];
+    let node: TrieNode = this.root;
+    for (let char of word) {
+      if (node.children.has(char)) {
+        node = node.children.get(char)!;
+      } else {
+        return res;
+      }
+    }
+    this.helper(node, res, word.substring(0, word.length - 1));
+    return res;
+  }
+
+  helper(node: TrieNode, res: string[], prefix: string): void {
+    if (node.isEnd) {
+      res.push(prefix + node.data);
+    }
+    for (let char of node.children.keys()) {
+      this.helper(node.children.get(char)!, res, prefix + node.data);
+    }
+  }
+}
+
+const t = new Trie();
+t.insert("car");
+t.insert("carpet");
+t.insert("java");
+t.insert("javascript");
+t.insert("internet");
+
+console.log(t.autocomplete("ja"));
