cb2deb51e228a838f46a5c7cf1460e1f31bc317d
Frosenow <filip.potepa@gmail.com>
2023-04-30 17:17:05 +0200

    created methods and styles for alert component

--- Kahoot!_Challenge/client/src/components/Alert.tsx
+++ Kahoot!_Challenge/client/src/components/Alert.tsx
@@ -1,15 +1,7 @@
-import { useState, useEffect } from "react";
-
 export default function Alert({ message }: { message: string }) {
-  const [show, setShow] = useState(true);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setShow(false);
-    }, 3000);
-
-    return () => clearTimeout(timer);
-  }, []);
-
-  return <>{show && <div className="alert">{message}</div>}</>;
+  return (
+    <div className="alert">
+      <span className="alert-msg">{message}</span>
+    </div>
+  );
 }
