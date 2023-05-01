1bda954c43a6625262aa6fc1a9d5cca77c13911f
Frosenow <filip.potepa@gmail.com>
2023-05-01 13:18:52 +0200

    App deployment package generated, ready for deployment

--- Kahoot!_Challenge/server/server.js
+++ Kahoot!_Challenge/server/server.js
@@ -1,20 +1,32 @@
 const fs = require("fs");
 const express = require("express");
 
+// Set the port to listen on, using either the environment variable or a default of 5000
 const PORT = process.env.PORT || 5000;
+
+// Create an instance of the Express application
 const app = express();
 
+// Serve static files from the specified directory
+app.use(express.static("../client/dist"));
+
+// Handle GET requests to the /dictionary route
 app.get("/dictionary", async (req, res) => {
+  // Read the contents of the dictionary file
   const words = await fs.readFileSync("./assets/english.txt", (err, data) => {
     if (err) {
+      // If there is an error, return a 500 error status
       res.status(500).send(err);
     }
     return data;
   });
+  // Split the contents of the file into an array of words
   const wordsArr = words.toString().split(/\r?\n/);
+  // Send the array of words as a JSON string in the response
   res.send(JSON.stringify(wordsArr));
 });
 
+// Start the server listening on the specified port
 app.listen(PORT, () => {
   console.log(`Server running on PORT: ${PORT}`);
 });
